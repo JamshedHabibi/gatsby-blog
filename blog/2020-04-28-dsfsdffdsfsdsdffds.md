e6d0f4a7a7b514ee4e48a61494295ac2cfa8c0e0
Jamshed Habibi <jamshed.habibi100@gmail.com>
2020-04-28 11:27:55 +0100

    change from md to json for blog posts

--- blog/2020-04-28-dsfsdffdsfsdsdffds.md
+++
@@ -1,6 +0,0 @@
----
-path: dsfdfsdsfdsdfds
-date: 2020-04-28T08:40:34.868Z
-title: dsfsdffdsfsdsdffds
----
-sdfsddfsdfsdsdfsdfdfs
--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `blogs`,
+        path: `${__dirname}/blog`,
       },
     },
     {
