982bb04e51371bac07e0dde8642fd3bd1cc8a2f3
Jamshed Habibi <jamshed.habibi100@gmail.com>
2020-06-27 15:53:17 +0100

    added categories navbar

--- src/components/layout/navbar/nav-tabs-layout.tsx
+++ src/components/layout/navbar/nav-tabs-layout.tsx
@@ -14,6 +14,7 @@ const navTabStyle = css([
     fontSize: ".85rem",
     textDecoration: "none",
     transition: "all 0.2s ease-in-out",
+    cursor: "pointer",
     "&:hover": {
       color: colors.highlightNavbarText,
     },
@@ -21,38 +22,50 @@ const navTabStyle = css([
 ])
 
 type NavTabProps = {
-  path: string
+  path?: string
   displayText: string
   showArrow?: boolean
 }
 
-export const NavTabLayout: React.FC<any> = () => {
+export const NavTabLayout: React.FC<any> = () => (
+  <div
+    css={[
+      flex.rowCentered,
+      {
+        width: "100%",
+      },
+    ]}
+  >
+    <div
+      css={mq({
+        display: "flex",
+        justifyContent: "space-around",
+        width: ["100%", "40%"],
+      })}
+    >
+      <NavTab path="/" displayText="home" />
+      <NavTab path="/about" displayText="about" />
+      <NavDropdownTab displayText="categories" />
+      <NavDropdownTab displayText="archive" />
+    </div>
+  </div>
+)
+
+const NavTab: React.FC<NavTabProps> = ({ displayText, path }) => {
   return (
     <div
-      css={[
-        flex.rowCentered,
-        {
-          width: "100%",
-        },
-      ]}
+      css={mq({
+        padding: ".5rem",
+      })}
     >
-      <div
-        css={mq({
-          display: "flex",
-          justifyContent: "space-around",
-          width: ["100%", "40%"],
-        })}
-      >
-        <NavTab path="/" displayText="Home" />
-        <NavTab path="/about" displayText="About" />
-        <NavTab path="/categories" displayText="Categories" showArrow />
-        <NavTab path="/archive" displayText="Archive" showArrow />
-      </div>
+      <Link css={navTabStyle} to={path}>
+        {displayText.toUpperCase()}
+      </Link>
     </div>
   )
 }
 
-class NavTab extends Component<NavTabProps, { showArrowUp: boolean }> {
+class NavDropdownTab extends Component<NavTabProps, { showArrowUp: boolean }> {
   constructor(props: NavTabProps) {
     super(props)
     this.state = {
@@ -60,39 +73,76 @@ class NavTab extends Component<NavTabProps, { showArrowUp: boolean }> {
     }
   }
 
-  showArrowUp = () => {
-    this.setState({ showArrowUp: !this.state.showArrowUp })
+  activateDropdownMenu = () => {
+    this.setState({ showArrowUp: !this.state.showArrowUp }, () => {
+      if (this.state.showArrowUp === true) {
+        document.getElementById(
+          `${this.props.displayText}-dropdown`
+        ).style.top = "8vh"
+      } else {
+        document.getElementById(
+          `${this.props.displayText}-dropdown`
+        ).style.top = "-30vh"
+      }
+    })
   }
 
   render() {
+    const { displayText } = this.props
     return (
-      <div
-        css={mq({
-          padding: ".5rem",
-        })}
-      >
-        {this.props.showArrow ? (
+      <>
+        <div
+          css={[
+            navTabStyle,
+            {
+              padding: ".5rem",
+              zIndex: 2,
+              display: "flex",
+              justifyContent: "center",
+            },
+          ]}
+          onClick={() => this.activateDropdownMenu()}
+        >
+          {this.props.displayText.toUpperCase()}
+          <FontAwesomeIcon
+            css={{
+              fontSize: "0.6rem",
+              paddingLeft: ".4rem",
+              paddingTop: ".4rem",
+            }}
+            icon={`${this.state.showArrowUp ? "chevron-up" : "chevron-down"}`}
+          />
           <div
-            css={navTabStyle}
-            onMouseOver={() => this.showArrowUp()}
-            onMouseOut={() => this.showArrowUp()}
+            id={`${displayText}-dropdown`}
+            css={[
+              {
+                position: "absolute",
+                top: "-30vh",
+                transition: "all 0.2s ease-in-out",
+                zIndex: 1,
+              },
+            ]}
           >
-            {this.props.displayText.toUpperCase()}
-            <FontAwesomeIcon
-              css={{
-                fontSize: "0.6rem",
-                paddingLeft: ".4rem",
-                paddingBottom: ".05rem",
-              }}
-              icon={`${this.state.showArrowUp ? "chevron-up" : "chevron-down"}`}
-            />
+            {displayText === "categories" ? (
+              <div
+                css={{
+                  display: "flex",
+                  background: colors.mainBabyBlue,
+                  padding: ".5rem",
+                  width: "25rem",
+                  justifyContent: "space-around",
+                }}
+              >
+                <NavTab displayText="Travel" path="/blog/travel" />
+                <NavTab displayText="Lifestyle" path="/blog/lifestyle" />
+                <NavTab displayText="Bookish" path="/blog/bookish" />
+              </div>
+            ) : (
+              <div></div>
+            )}
           </div>
-        ) : (
-          <Link css={navTabStyle} to={this.props.path}>
-            {this.props.displayText.toUpperCase()}
-          </Link>
-        )}
-      </div>
+        </div>
+      </>
     )
   }
 }
